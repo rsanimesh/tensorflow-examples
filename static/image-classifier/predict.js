7b5244b4b6e3390c7196241291e8cb3551ada87f
Rahul Kumar Sinha <rsanimesh@gmail.com>
2019-05-28 15:57:11 +0530

    added progressbar and model selector

--- static/image-classifier/predict.js
+++ static/image-classifier/predict.js
@@ -9,21 +9,24 @@ $("#image-selector").change(function(){
     reader.readAsDataURL(file);
 });
 
+$("#model-selector").change(function(){
+    loadModel($("#model-selector").val());
+});
+
 let model;
-(async function(){
-    model = await tf.loadLayersModel("/tfjs-models/MOBILENET/model.json")
-    $(".progress-bar").hide();
-})();
+async function loadModel(name){
+    $("#progressbar").show();
+    model = undefined;
+    model = await tf.loadLayersModel(`/tfjs-models/${name}/model.json`);
+    $("#progressbar").hide();
+}
 
 $("#predict-button").click(async function (){
+    $("#predictionProgressBar").show();
     let image = $("#selected-image").get(0);
-    let tensor = tf.browser.fromPixels(image)
-    .resizeNearestNeighbor([224,224])
-    .toFloat()
-    .expandDims();
-
-    // More pre-processing to be added here later
-
+    let modelName = $("#model-selector").val();
+    let tensor = preprocessImage(image, modelName);
+    
     let predictions = await model.predict(tensor).data();
 
     let top5 = Array.from(predictions)
@@ -40,6 +43,32 @@ $("#predict-button").click(async function (){
     top5.forEach(function (p){
         $("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
     });
+    $("#predictionProgressBar").hide();
 
 
-})
+});
+
+function preprocessImage(image, modelName){
+    let tensor = tf.browser.fromPixels(image)
+    .resizeNearestNeighbor([224,224])
+    .toFloat()
+
+    if (modelName === undefined){
+        return tensor.expandDims();
+    }
+    else if (modelName === "VGG16"){
+        let meanImageNetRGB = tf.tensor1d([123.68, 116.779, 103.939]);
+        return tensor.sub(meanImageNetRGB)
+        .reverse(2)
+        .expandDims();
+    }
+    else if(modelName === "MobileNet"){
+        let offset = tf.scalar(127.5);
+        return tensor.sub(offset)
+        .div(offset)
+        .expandDims();
+    }
+    else {
+        throw new Error("Unknown model name");
+    }
+};
